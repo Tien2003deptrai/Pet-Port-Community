23bfc45532017480cc479eb484f4ad5eadea5a3b
Tien2003deptrai <duongvantiendtu@gmail.com>
2024-10-11 14:36:04 +0700

    Clear Git cache and apply .gitignore

--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('./database');
-const upload = require('./multer');
-const { transporter } = require('./mail');
-
-module.exports = {
-  sequelize,
-  upload,
-  transporter,
-};
--- src/middlewares/authMiddleware.js
+++
@@ -1,5 +0,0 @@
-const passport = require('passport');
-
-exports.authenticateJWT = passport.authenticate('jwt', {
-  session: false,
-});
