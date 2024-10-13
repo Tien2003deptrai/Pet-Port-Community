c7af59498297822ea3dfff093ea4f9a6ba6e55ad
Tien2003deptrai <duongvantiendtu@gmail.com>
2024-10-13 19:28:34 +0700

    cors + login

--- src/utils/corsOptions.js
+++ src/utils/corsOptions.js
@@ -1,5 +1,5 @@
 exports.corsOptions = {
-  origin: 'http://localhost:5501',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
