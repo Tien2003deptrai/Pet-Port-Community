def81479561fbf044d71c818ce37a4da13f67baf
Tien2003deptrai <duongvantiendtu@gmail.com>
2024-11-10 12:55:18 +0700

    fix and eslint

--- src/config/mail.js
+++ src/config/mail.js
@@ -2,10 +2,10 @@ const nodemailer = require('nodemailer');
 
 exports.transporter = nodemailer.createTransport({
   service: 'gmail',
-  host: process.env.SMTP_HOST || "smtp.gmail.com",
+  host: process.env.SMTP_HOST || 'smtp.gmail.com',
   port: process.env.SMTP_PORT || 587,
   auth: {
-    user: process.env.SMTP_USER || "trungducloz11111@gmail.com",
-    pass: process.env.SMTP_PASS || "lwrs nhre roew efns",
+    user: process.env.SMTP_USER || 'trungducloz11111@gmail.com',
+    pass: process.env.SMTP_PASS || 'lwrs nhre roew efns',
   },
 });
